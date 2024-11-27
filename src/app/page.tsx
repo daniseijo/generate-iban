f17adc0e2ed4e234409a5ada263fc6aead88da1a
Daniel Seijo <daniseijo.dev@gmail.com>
2024-11-27 19:58:58 +0100

    refactor: format and style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,13 @@ import { ModeToggle } from './component/ModeToggle'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <ModeToggle />
-      <Button>Click me</Button>
+    <div className="flex min-h-screen flex-col">
+      <header className="flex justify-end">
+        <ModeToggle className="m-4" />
+      </header>
+      <main className="flex flex-col items-center justify-center">
+        <Button className="mt-16">Click me</Button>
+      </main>
     </div>
   )
 }
