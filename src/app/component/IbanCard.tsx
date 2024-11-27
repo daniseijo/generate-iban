1b02ef9035e8820a896c46a88f1cc0c4f2922370
Daniel Seijo <daniseijo.dev@gmail.com>
2024-11-27 21:35:13 +0100

    feat: connect state

--- src/app/component/IbanCard.tsx
+++ src/app/component/IbanCard.tsx
@@ -5,13 +5,50 @@ import { Input } from '@/components/ui/input'
 import { cn } from '@/lib/utils'
 import { Separator } from '@/components/ui/separator'
 import { CopyButton } from './CopyButton'
+import { useEffect, useState } from 'react'
+import { spanishAccountNumberToIBAN } from '@/lib/cccToIban'
 
 export type IbanCardProps = {
   className?: string
 }
 
 export function IbanCard({ className }: IbanCardProps) {
-  const iban = 'ES63 0081 5168 80 5183341899'
+  const [bank, setBank] = useState('')
+  const [entity, setEntity] = useState('')
+  const [account, setAccount] = useState('')
+  const [ccc, setCcc] = useState('')
+  const [iban, setIban] = useState('')
+
+  useEffect(() => {
+    if (ccc.length === 18 && !ccc.includes('X')) {
+      const iban = spanishAccountNumberToIBAN(ccc.slice(0, 4), ccc.slice(4, 8), ccc.slice(8, 18))
+      setIban(`${iban.slice(0, 4)} ${iban.slice(4, 8)} ${iban.slice(8, 12)} ${iban.slice(12, 14)} ${iban.slice(14)}`)
+    } else {
+      setIban('')
+    }
+  }, [ccc])
+
+  const handleBankChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setBank(event.target.value)
+    const bankValue = event.target.value.padEnd(4, 'X')
+    setCcc(bankValue + ccc.slice(4, 18))
+  }
+
+  const handleEntityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEntity(event.target.value)
+    const entityValue = event.target.value.padEnd(4, 'X')
+    setCcc(bank + entityValue + ccc.slice(8, 18))
+  }
+  const handleAccountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAccount(event.target.value)
+    setCcc(bank + entity + event.target.value.padEnd(10, 'X'))
+  }
+  const handleCccChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCcc(event.target.value)
+    setBank(event.target.value.slice(0, 4))
+    setEntity(event.target.value.slice(4, 8))
+    setAccount(event.target.value.slice(8, 18))
+  }
 
   return (
     <Card className={cn('w-96', className)}>
@@ -26,26 +63,38 @@ export function IbanCard({ className }: IbanCardProps) {
           <div className="text-sm">Pega aquí los datos de tu cuenta:</div>
           <div className="flex w-full items-center gap-2">
             <div className="basis-1/3 flex flex-col space-y-1">
-              <Input maxLength={4} id="bank" placeholder="Banco" />
+              <Input maxLength={4} id="bank" placeholder="Banco" value={bank} onChange={handleBankChange} />
             </div>
             <div className="basis-1/3 flex flex-col space-y-1">
-              <Input maxLength={4} id="entity" placeholder="Entidad" />
+              <Input maxLength={4} id="entity" placeholder="Entidad" value={entity} onChange={handleEntityChange} />
             </div>
             <div className="basis-2/3 flex flex-col space-y-1">
-              <Input maxLength={10} id="account" placeholder="Número de cuenta" />
+              <Input
+                maxLength={10}
+                id="account"
+                placeholder="Número de cuenta"
+                value={account}
+                onChange={handleAccountChange}
+              />
             </div>
           </div>
           <div className="text-sm">O bien, pega aquí directamente el CCC:</div>
           <div className="flex flex-col space-y-1">
-            <Input maxLength={18} placeholder="Código de la cuenta cliente" />
+            <Input maxLength={18} placeholder="Código de la cuenta cliente" value={ccc} onChange={handleCccChange} />
           </div>
-          <Separator />
-          <div className="text-center text-lg font-semibold">{iban}</div>
+          {iban && (
+            <>
+              <Separator />
+              <div className="text-center text-lg font-semibold">{iban}</div>
+            </>
+          )}
         </div>
       </CardContent>
-      <CardFooter className="flex justify-end">
-        <CopyButton text={iban} />
-      </CardFooter>
+      {iban && (
+        <CardFooter className="flex justify-end">
+          <CopyButton text={iban} />
+        </CardFooter>
+      )}
     </Card>
   )
 }
